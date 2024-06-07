78695b63018bd78240a99afa8c0f07914543d53e
Zi-Liang <f10245003@ntu.edu.tw>
2024-06-07 14:19:15 +0800

    construct the new format rule of fort.9 properly run the input part in main program

--- MultPlane3-1.py
+++ MultPlane3-1.py
@@ -64,7 +64,7 @@ SURF = {
     "ECENT": np.zeros((NARDIM, 2))
 }
 
-CD = {
+CD_BLOCK = {
     "EF": 0,
     "ESTART": 0,
     "DELE": 0,
@@ -80,17 +80,17 @@ TIPPOS = {
     "Y0": 0
 }
 
-# Data input from fort.9 file
+# Function to read fort_new.9 file
 
 
-def read_fort9(filename="fort.9"):
+def read_fort9(filename="fort_new.9"):
     with open(filename, 'r') as file:
         data = file.readlines()
 
+    # 去除注釋部分
     data = [line.split()[0] for line in data if line.strip()
             and not line.lstrip().startswith('C')]
 
-    # print(data)
     index = 0
 
     NPARM = int(data[index].strip())
@@ -185,11 +185,263 @@ def read_fort9(filename="fort.9"):
         param["ISTK"] = int(data[index].strip())
         index += 1
 
+        param["MIRROR"] = int(data[index].strip())
+        index += 1
+
+        param["NRIN"] = int(data[index].strip())
+        index += 1
+        param["NVIN"] = int(data[index].strip())
+        index += 1
+        param["NSIN"] = int(data[index].strip())
+        index += 1
+        param["NPIN"] = int(data[index].strip())
+        index += 1
+        param["SIZE"] = float(data[index].strip())
+        index += 1
+        if param["SIZE"] <= 0:
+            param["DELRIN"] = float(data[index].strip())
+            index += 1
+            param["DELSIN"] = float(data[index].strip())
+            index += 1
+
+        param["IPMAX"] = int(data[index].strip())
+        index += 1
+        # 將 ITMAX 的值從逗號分隔的字符串轉換為浮點數列表，過濾掉空字符串
+        param["ITMAX"] = [int(float(x))
+                          for x in data[index].split(',') if x.strip()]
+        index += 1
+
+        # 將 EP 的值從逗號分隔的字符串轉換為浮點數列表，過濾掉空字符串
+        param["EP"] = [float(x) for x in data[index].split(',') if x.strip()]
+        index += 1
+
+        param["NE"] = int(data[index].strip())
+        index += 1
+
+        param["IWRIT"] = int(data[index].strip())
+        index += 1
+
+        # 檢查下一行是否包含逗號分隔的數值串並處理 NBIAS 和 BBIAS
+
+        param["NBIAS"] = int(data[index].strip())
+        index += 1
+
+        # Read BBIAS values from the next line
+        # change '2.0, 1.9, 1.8 ...' string to list of floats
+        param["BBIAS"] = [float(x)
+                          for x in data[index].split(',') if x.strip()]
+        index += 1
+
+        param["NUMC"] = int(data[index].strip())
+        index += 1
+        param["DELPOT"] = float(data[index].strip())
+        index += 1
+        param["PhiIN"] = float(data[index].strip())
+        index += 1
+        param["CHI"] = float(data[index].strip())
+        index += 1
+        param["EFTIP"] = float(data[index].strip())
+        index += 1
+        param["VACWID"] = float(data[index].strip())
+        index += 1
+        param["ZVACDEL"] = float(data[index].strip())
+        index += 1
+        param["EMAX"] = [float(x) for x in data[index].split(',') if x.strip()]
+        index += 1
+        param["ICOMPIN"] = int(data[index].strip())
+        index += 1
+        param["BMOD"] = float(data[index].strip())
+        index += 1
+        param["ANEG"] = float(data[index].strip())
+        index += 1
+        param["APOS"] = float(data[index].strip())
+        index += 1
+        param["VSTART"] = float(data[index].strip())
+        index += 1
+        if param["VSTART"] < 0:
+            param["DELS1"] = abs(param["VSTART"]) * param["ANEG"]
+        else:
+            param["DELS1"] = abs(param["VSTART"]) * param["APOS"]
+        param["SEP0"] = param["SEPIN"] - param["DELS1"]
+
         parameters.append(param)
 
     return parameters
 
+# Function to process the parameters and initialize common blocks and arrays
+
+
+def process_parameters(parameters):
+    # 初始化 CC 和 SEMI 字典
+    CC = {
+        "CD": np.zeros(NREGDIM),
+        "CA": np.zeros(NREGDIM)
+    }
+    SEMI = {
+        "EGAP": np.zeros(NREGDIM),
+        "DELVB": np.zeros(NREGDIM),
+        "ED": np.zeros(NREGDIM),
+        "EA": np.zeros(NREGDIM),
+        "ACB": np.zeros(NREGDIM),
+        "AVB": np.zeros(NREGDIM),
+        "IDEG": np.zeros(NREGDIM, dtype=int),
+        "IINV": np.zeros(NREGDIM, dtype=int)
+    }
+
+    for param in parameters:
+        SLOPE = param["SLOPE"]
+        SEPIN = param["SEPIN"]
+        RAD = param["RAD"]
+        RAD2 = param["RAD2"]
+        CPot = param["CPot"]
+        X0 = param["X0"]
+        Y0 = param["Y0"]
+        NREG = param["NREG"]
+
+        print(
+            f"RAD, SLOPE, ANGLE = {RAD}, {SLOPE}, {360.0 * np.arctan(1.0 / SLOPE) / PI}")
+        print(f"CONTACT POTENTIAL = {CPot}")
+        print(f"POSITION OF TIP = {X0}, {Y0}")
+        print(f"NUMBER OF DIFFERENT REGIONS OF SEMICONDUCTOR = {NREG}")
+
+        if NREG > NREGDIM:
+            print("INPUT NUMBER OF REGIONS > NUMBER OF REGIONS IN PARAMETER STATEMENT")
+            print("PROGRAM WILL BE EXITED (TYPE RETURN)")
+            input()
+            exit()
+
+        for ireg, region in enumerate(param["REGIONS"]):
+            if NREG > 1:
+                print(f"REGION #{ireg + 1}")
+
+            CC["CD"][ireg] = region["CD"]
+            CC["CA"][ireg] = region["CA"]
+            SEMI["EGAP"][ireg] = region["EGAP"]
+            SEMI["DELVB"][ireg] = region["DELVB"]
+            SEMI["ED"][ireg] = region["ED"]
+            SEMI["EA"][ireg] = region["EA"]
+            SEMI["ACB"][ireg] = region["ACB"]
+            AVBH[ireg] = region["AVBH"]
+            AVBL[ireg] = region["AVBL"]
+            SEMI["AVB"][ireg] = np.exp(
+                2.0 * np.log(np.sqrt(AVBH[ireg]**3) + np.sqrt(AVBL[ireg]**3)) / 3.0)
+            AVBSO[ireg] = region["AVBSO"]
+            ESO[ireg] = region["ESO"]
+            SEMI["IDEG"][ireg] = region["IDEG"]
+            SEMI["IINV"][ireg] = region["IINV"]
+
+            print(f"DOPING = {CC['CD'][ireg]}, {CC['CA'][ireg]}")
+            print(
+                f"BAND GAP, VB OFFSET = {SEMI['EGAP'][ireg]}, {SEMI['DELVB'][ireg]}")
+
+            if (CC["CA"][ireg] > CC["CD"][ireg] and (SEMI["IINV"][ireg] == 1 or SEMI["IINV"][ireg] == 3)) or \
+               (CC["CD"][ireg] > CC["CA"][ireg] and (SEMI["IINV"][ireg] == 2 or SEMI["IINV"][ireg] == 3)):
+                print(
+                    "****** WARNING - LIKELY INCOMPATIBLE DOPING AND INVERSION (IINV) PARAMETER")
+                print("CONTINUE (y/n) ?")
+                ans = input().strip().lower()
+                if ans != 'y':
+                    exit()
+
+        EPSIL = param["EPSIL"]
+        TEM = param["TEM"]
+        TK = param["TK"]
+        TK1 = param["TK1"]
+
+        NAR = param["NAR"]
+        print(f"NUMBER OF DIFFERENT AREAS OF SURFACE STATES = {NAR}")
+
+        if NAR > NARDIM:
+            print("INPUT NUMBER OF AREAS > NUMBER OF AREAS IN PARAMETER STATEMENT")
+            print("PROGRAM WILL BE EXITED (TYPE RETURN)")
+            input()
+            exit()
+
+        for iar, area in enumerate(param["AREAS"]):
+            if NAR > 1:
+                print(f"AREA #{iar + 1}")
+
+            SURF["DENS"][iar, 0] = area["DENS1"]
+            SURF["EN"][iar, 0] = area["EN1"]
+            SURF["FWHM"][iar, 0] = area["FWHM1"]
+            SURF["ECENT"][iar, 0] = area["ECENT1"]
+            SURF["DENS"][iar, 1] = area["DENS2"]
+            SURF["EN"][iar, 1] = area["EN2"]
+            SURF["FWHM"][iar, 1] = area["FWHM2"]
+            SURF["ECENT"][iar, 1] = area["ECENT2"]
+
+            print("FIRST DISTRIBUTION OF SURFACE STATES:")
+            print(
+                f"SURFACE STATE DENSITY, EN = {SURF['DENS'][iar, 0]}, {SURF['EN'][iar, 0]}")
+            print(
+                f"FWHM, ECENT = {SURF['FWHM'][iar, 0]}, {SURF['ECENT'][iar, 0]}")
+            print("SECOND DISTRIBUTION OF SURFACE STATES:")
+            print(
+                f"SURFACE STATE DENSITY, EN = {SURF['DENS'][iar, 1]}, {SURF['EN'][iar, 1]}")
+            print(
+                f"FWHM, ECENT = {SURF['FWHM'][iar, 1]}, {SURF['ECENT'][iar, 1]}")
+
+        SURF["ISTK"] = param["ISTK"]
+
+        # 初始化其它參數
+        MIRROR = param["MIRROR"]
+        if MIRROR == 1:
+            print("HORIZONTAL MIRROR PLANE ASSUMED")
+
+        NRIN = param["NRIN"]
+        NVIN = param["NVIN"]
+        NSIN = param["NSIN"]
+        NPIN = param["NPIN"]
+        SIZE = param["SIZE"]
+        if SIZE <= 0:
+            DELRIN = param["DELRIN"]
+            DELSIN = param["DELSIN"]
+
+        IPMAX = param["IPMAX"]
+        ITMAX = param["ITMAX"]
+        EP = param["EP"]
+        NE = param["NE"]
+
+        # 找出整體電荷中性水平 EN0
+        EN0 = np.zeros(NARDIM)
+        for IAR in range(NAR):
+            if SURF["DENS"][IAR, 0] == 0 and SURF["DENS"][IAR, 1] == 0:
+                EN0[IAR] = 0.0
+            elif SURF["DENS"][IAR, 0] == 0:
+                EN0[IAR] = SURF["EN"][IAR, 1]
+            elif SURF["DENS"][IAR, 1] == 0:
+                EN0[IAR] = SURF["EN"][IAR, 0]
+            else:
+                print("SEARCHING FOR CHARGE NEUTRALITY LEVEL")
+                # 呼叫 ENFIND 子程序
+                EN0[IAR] = (SURF["EN"][IAR, 0] + SURF["EN"]
+                            [IAR, 1]) / 2  # 模擬計算
+
+            print(f"CHARGE-NEUTRALITY LEVEL = {EN0[IAR]}")
+
+        EN0MAX = np.max(EN0)
+        EN0MIN = np.min(EN0)
+
+        IWRIT = param["IWRIT"]
+        NBIAS = param["NBIAS"]
+        BBIAS = param["BBIAS"]
+        NUMC = param["NUMC"]
+        DELPOT = param["DELPOT"]
+        PhiIN = param["PhiIN"]
+        CHI = param["CHI"]
+        EFTIP = param["EFTIP"]
+        VACWID = param["VACWID"]
+        ZVACDEL = param["ZVACDEL"]
+        EMAX = param["EMAX"]
+        ICOMPIN = param["ICOMPIN"]
+        BMOD = param["BMOD"]
+        ANEG = param["ANEG"]
+        APOS = param["APOS"]
+        VSTART = param["VSTART"]
+        DELS1 = param["DELS1"]
+        SEP0 = param["SEP0"]
+
 
 # Example usage
-parameters = read_fort9("fort.9")
-print(parameters)
+parameters = read_fort9("fort_new.9")
+process_parameters(parameters)
